5d2708c714cd0e086b2664987a632e8e004a84ac
matangeorgi <matang@wix.com>
2022-11-28 19:03:37 +0200

    migrate completed lessons into codux-demos

--- src/components/lessons/05/lesson-05.tsx
+++ src/components/lessons/05/lesson-05.tsx
@@ -1,18 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import type React from 'react';
 import styles from './lesson-05.module.scss';
 import { Box } from '../../common/box/box';
-import { ConfettiFx } from '../../fx/confetti-fx/confetti-fx';
 import { Task05 as Task } from '../../tasks/05/task-05';
 
 export const Lesson05: React.FC = () => {
-    const [lessonSolved, setLessonSolved] = useState(false);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setLessonSolved(isSolved());
-        }, 750);
-    }, []);
-
     return (
         <div className={styles.root}>
             <Task />
@@ -43,7 +34,7 @@ export const Lesson05: React.FC = () => {
                     </div>
                 </div>
                 <div className={styles.section}>
-                    <div className={styles.row} >
+                    <div className={styles.row}>
                         <Box
                             text="Give me some space"
                             color={'turquoiseGreen'}
@@ -52,31 +43,7 @@ export const Lesson05: React.FC = () => {
                         />
                     </div>
                 </div>
-                <ConfettiFx
-                    maxParticles={400}
-                    dissolve={315}
-                    show={lessonSolved}
-                    style={{ display: lessonSolved ? 'block' : 'none' }}
-                />
             </div>
         </div>
     );
 };
-
-function isSolved(): boolean {
-    const margin = getNumValue(
-        getComputed('margin').getPropertyValue('margin')
-    );
-    const padding = getNumValue(
-        getComputed('padding').getPropertyValue('padding')
-    );
-    return margin <= 6 && margin >= 0 && padding <= 20 && padding >= 10;
-}
-
-function getComputed(id: string) {
-    return getComputedStyle(document.getElementById(id)!);
-}
-
-function getNumValue(cssValue: string): number {
-    return Number(cssValue.replace(/px$/, ''));
-}
--- src/components/lessons/08/lesson-08.tsx
+++ src/components/lessons/08/lesson-08.tsx
@@ -1,16 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import type React from 'react';
 import styles from './lesson-08.module.scss';
 import { Box } from '../../common/box/box';
-import { ConfettiFx } from '../../fx/confetti-fx/confetti-fx';
 import { Task08 as Task } from '../../tasks/08/task-08';
 
 export const Lesson08: React.FC = () => {
-    const [lessonSolved, setLessonSolved] = useState(false);
-
-    useEffect(() => {
-        setLessonSolved(isSolved());
-    }, []);
-
     return (
         <div className={styles.root}>
             <Task />
@@ -46,32 +39,7 @@ export const Lesson08: React.FC = () => {
                         className={styles.tag}
                     />
                 </div>
-                <ConfettiFx
-                    maxParticles={400}
-                    dissolve={315}
-                    show={lessonSolved}
-                    style={{ display: lessonSolved ? 'block' : 'none' }}
-                />
             </div>
         </div>
     );
 };
-
-function isSolved(): boolean {
-    const elem = getComputed('circles');
-    const isflex = elem.getPropertyValue('display') === 'flex';
-    const isSolvedDirection =
-        elem.getPropertyValue('flex-direction') === 'column';
-    const isSolvedJustify =
-        elem.getPropertyValue('justify-content') === 'flex-end' || elem.getPropertyValue('justify-content') === 'end';
-    const isSolvedGap = getNumValue(elem.getPropertyValue('gap')) === 26;
-    return isflex && isSolvedDirection && isSolvedJustify && isSolvedGap;
-}
-
-function getComputed(id: string) {
-    return getComputedStyle(document.getElementById(id)!);
-}
-
-function getNumValue(cssValue: string): number {
-    return Number(cssValue.replace(/px$/, ''));
-}
