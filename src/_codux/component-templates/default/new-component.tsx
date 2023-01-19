91da88ce8d6b645593a31e8f377733977bd9331a
Sagiv Dayan <67858097+TheAlmightyCrumb@users.noreply.github.com>
2023-01-19 14:36:01 +0200

    feat: add classnames dep and use in new component (#18)

--- src/_codux/component-templates/default/new-component.tsx
+++ src/_codux/component-templates/default/new-component.tsx
@@ -1,9 +1,12 @@
 import styles from './new-component.module.scss';
+import classNames from 'classnames';
 
 export interface NewComponentProps {
     className?: string;
 }
 
 export const NewComponent = ({ className }: NewComponentProps) => {
-    return <div className={`${styles.root} ${className}`}>NewComponent</div>;
+    return (
+        <div className={classNames(styles.root, className)}>NewComponent</div>
+    );
 };
