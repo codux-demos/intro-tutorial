35424fb81d19bf4bfa510a3e26d9176db27a09d8
Peter Shershov <peters@wix.com>
2023-01-30 13:04:21 +0200

    fix(types): type errors in completed lessons

--- src/_codux/boards/lesson-02/lesson-02-board.board.tsx
+++ src/_codux/boards/lesson-02/lesson-02-board.board.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { createBoard } from '@wixc3/react-board';
 import { Lesson02 } from '../../../components/lessons/02/lesson-02';
 
--- src/_codux/boards/lesson-05/lesson-05-board.board.tsx
+++ src/_codux/boards/lesson-05/lesson-05-board.board.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { createBoard } from '@wixc3/react-board';
 import { Lesson05 } from '../../../components/lessons/05/lesson-05';
 
