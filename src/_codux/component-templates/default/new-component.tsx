425b82a1a1632cdacfa7d9b4adcc02f1dcee99b3
Sagiv Dayan <sagiv.keeper@gmail.com>
2023-09-07 00:33:23 +0300

    chore(infra): vite migration

--- src/_codux/component-templates/default/new-component.tsx
+++ src/_codux/component-templates/default/new-component.tsx
@@ -1,18 +1,14 @@
-import styles from './new-component.module.scss';
 import classNames from 'classnames';
+import styles from './new-component.module.scss';
 
 export interface NewComponentProps {
     className?: string;
-    children?: React.ReactNode;
 }
 
 /**
- * This component was generated using Codux's built-in Default new component template.
- * For details on on how to create custom new component templates, see https://help.codux.com/kb/en/article/configuration-for-new-components-and-templates
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
-export const NewComponent = ({
-    className,
-    children = 'New Component',
-}: NewComponentProps) => {
-    return <div className={classNames(styles.root, className)}>{children}</div>;
+export const NewComponent = ({ className }: NewComponentProps) => {
+    return <div className={classNames(styles.root, className)}>NewComponent</div>;
 };
