5a837a75fb1a823de898cdcab16ea59e014c25c9
PeterShershov <stelaviv@gmail.com>
2023-10-24 11:11:22 +0300

    formatting

--- src/components/tasks/05/task-05.tsx
+++ src/components/tasks/05/task-05.tsx
@@ -11,31 +11,29 @@ export const Task05 = () => {
             <div className={styles.title}>Styles Panel</div>
             <ol start={0} className={styles.desc}>
                 <li>
-                    On the <b>Styles Panel</b>, you can apply <b>styles</b> to
-                    one or more elements using
+                    On the <b>Styles Panel</b>, you can apply <b>styles</b> to one or more elements
+                    using
                     <b> selectors</b>. Let's use them to adjust spacing.
                 </li>
                 <li>
-                    To bring the circles closer together, we need to adjust
-                    their margins. <br></br> Edit
+                    To bring the circles closer together, we need to adjust their margins. <br></br>{' '}
+                    Edit
                     <TaskSymbol name="component" />
                     <b>Lesson05</b> and select any circle on the stage.
                 </li>
                 <li>
                     Now, open the
                     <TaskSymbol name="styles" />
-                    <b>Styles Panel</b>. Notice that you're editing the{' '}
-                    <TaskTag type="selector">.section .row .margin</TaskTag>{' '}
-                    selector.
+                    <b>Styles Panel</b>. Notice that you're editing the
+                    <TaskTag type="selector">.section .row .margin</TaskTag> selector.
                 </li>
                 <li>
-                    Scroll down to <b>Margin</b> and set it to <b>6px</b>. See
-                    how all circles change margins as they share the same
-                    selector.
+                    Scroll down to <b>Margin</b> and set it to <b>6px</b>. See how all circles
+                    change margins as they share the same selector.
                 </li>
                 <li>
-                    Next, let's adjust the padding of the button below. Select
-                    the button and then scroll to <b>Padding</b>. Set it to
+                    Next, let's adjust the padding of the button below. Select the button and then
+                    scroll to <b>Padding</b>. Set it to
                     <b> 15px</b>.
                 </li>
             </ol>
