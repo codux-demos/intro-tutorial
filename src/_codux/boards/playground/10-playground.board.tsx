af53a2dd781e32ce60540409b6d01a0e930fcf28
ShireeWill <shiree@wix.com>
2024-01-25 12:13:19 +0200

    feat add 6 different posters

--- src/_codux/boards/playground/10-playground.board.tsx
+++ src/_codux/boards/playground/10-playground.board.tsx
@@ -1,6 +1,8 @@
 import { createBoard } from '@wixc3/react-board';
 import styles from './playground.module.scss';
 import { Card } from '../../../components/card/card';
+import {Poster_01, Poster_02, Poster_03, Poster_04, Poster_05, Poster_06} from '../../../assets/posters';
+
 
 export default createBoard({
     name: '10 Playground',
@@ -13,36 +15,42 @@ export default createBoard({
                     price="$100"
                     title="Product Title"
                     className={styles.poster}
+                    imgSrc={Poster_01}
                 />
                 <Card
                     description="product description"
                     price="$100"
                     title="Product Title"
                     className={styles.poster}
+                    imgSrc={Poster_02}
                 />
                 <Card
                     description="product description"
                     price="$100"
                     title="Product Title"
                     className={styles.poster}
+                    imgSrc={Poster_03}
                 />
                 <Card
                     description="product description"
                     price="$100"
                     title="Product Title"
                     className={styles.poster}
+                    imgSrc={Poster_04}
                 />
                 <Card
                     description="product description"
                     price="$100"
                     title="Product Title"
                     className={styles.poster}
+                    imgSrc={Poster_05}
                 />
                 <Card
                     description="product description"
                     price="$100"
                     title="Product Title"
                     className={styles.poster}
+                    imgSrc={Poster_06}
                 />
             </div>
             <div className={styles.footer}>This is the footer</div>
---
+++ src/assets/posters/index.ts
@@ -0,0 +1,8 @@
+import Poster_01 from './poster_01.svg'
+import Poster_02 from './poster_02.svg';
+import Poster_03 from './poster_03.svg';
+import Poster_04 from './poster_04.svg';
+import Poster_05 from './poster_05.svg';
+import Poster_06 from './poster_06.svg';
+
+export {Poster_01, Poster_02, Poster_03, Poster_04, Poster_05, Poster_06};
