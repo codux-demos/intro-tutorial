d98ac132763b8867d63231122577e7c2ea841815
ShireeWill <shiree@wix.com>
2024-01-28 10:46:29 +0200

    formatting

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import styles from './card.module.scss';
+import Playground_module from '../../_codux/boards/playground/playground.module.scss';
 
 export interface CardProps {
     className?: string;
@@ -14,23 +15,33 @@ export interface CardProps {
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
-export const Card = ({ className, title, description, price, added = false, imgSrc }: CardProps) => {
+export const Card = ({
+    className,
+    title,
+    description,
+    price,
+    added = false,
+    imgSrc,
+}: CardProps) => {
     return (
         <div className={classNames(styles.root, className)}>
-            <img
-                src={imgSrc}
-                alt=""
-                className={styles.image}
-            />
-            <div>
+            <img src={imgSrc} alt="" className={styles.image} />
+            <div className={styles.layout}>
                 <div className={styles.cardetails}>
                     <div className={styles.details}>
                         <span className={styles.title}>Bauhaus </span>
                         <div className={styles.price}>
-                            <span>$6400</span></div></div>
+                            <span>$6400</span>
+                        </div>
+                    </div>
                     <span className={styles.description}>Bauhaus Bauhaus</span>
                 </div>
-                <button className={styles.button}>{added ? 'Remove From Cart' : 'Quickbuy'}</button></div>
+                <div>
+                    <button className={styles.button}>
+                        {added ? 'Remove From Cart' : 'Quickbuy'}
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
