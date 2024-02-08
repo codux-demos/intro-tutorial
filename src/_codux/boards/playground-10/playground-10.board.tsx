28de2fe04d8e2ceb1f0caa39231a974ef0a12d61
ShireeWill <shiree@wix.com>
2024-02-08 12:49:22 +0200

    MQ

--- src/_codux/boards/playground-10/playground-10.board.tsx
+++ src/_codux/boards/playground-10/playground-10.board.tsx
@@ -6,13 +6,13 @@ export default createBoard({
     Board: () => <Playground10 />,
     isSnippet: false,
     environmentProps: {
-        windowWidth: 1024,
+        windowWidth: 768,
         canvasMargin: {
             top: 0,
             right: 0,
             bottom: 0,
             left: 0,
         },
-        windowHeight: 768,
+        windowHeight: 1024,
     },
 });
