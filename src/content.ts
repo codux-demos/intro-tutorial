f1533d982c3bba57ff93c0eaa7387f7bab903c37
Eg0ld <80466939+Eg0ld@users.noreply.github.com>
2024-02-18 07:13:00 -0500

    Some suggested changes to text (#119)
    Co-authored-by: Codux <codux@codux.com>

--- src/content.ts
+++ src/content.ts
@@ -1,10 +1,6 @@
 export const content = {
-    componentHintText: 'This is a card component- \n a reusable piece of UI that lets you \n change properties & styles.',
-    fontHintText:'Too many fonts in the mix? \n Use the Styles Panel to edit \n and reuse fonts that better \n fit your design.',
+    componentHintText: 'This is a card component - \n a reusable piece of UI that lets you \n change properties & styles.',
+    fontHintText:'Too many fonts in the mix? \n Use the Styles panel to edit \n and reuse fonts that better \n fit your design.',
     layoutHintText:'Go wild and try out different \n layouts for this card.  \n \n  You’re the designer, you  \n call the shots  :-)',
-    buttonHintText:'This button color feels \n  like it’s disabled, how \n  will you change it?'
-
-
-
-
+    buttonHintText:'This button’s color feels \n  somewhat disabled. How \n  will you change it?'
 }
