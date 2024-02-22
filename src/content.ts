ae7433ca4e62b5d725068c2b74f17249cf80b593
mikawix <mikan@wix.com>
2024-02-22 12:02:15 +0200

    last hint text

--- src/content.ts
+++ src/content.ts
@@ -1,6 +1,12 @@
 export const content = {
     componentHintText: 'This is a card component - \n a reusable piece of UI that lets you \n change properties & styles.',
     fontHintText:'Too many fonts in the mix? \n Use the Styles panel to edit \n and reuse fonts that better \n fit your design.',
-    layoutHintText:'Go wild and try drag&drop elements \n to change the layout within the card.  \n \n  You’re the designer, you  \n call the shots  :-)',
+    layoutHintText:'Go ahead and drag & drop elements \n to change the layout. \n \n  You’re the designer, you  \n call the shots  :-)',
     buttonHintText:'This button’s color feels \n  somewhat disabled. How \n  will you change it?'
+
+
+
+
+
+
 }
