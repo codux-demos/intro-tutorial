8d51564a65deeb2e8d56abde5eda68077c9b7b8a
matan georgi <matang@wix.com>
2024-10-15 18:34:15 +0300

    lessons style

--- app/routes/_index/lessons.ts
+++ app/routes/_index/lessons.ts
@@ -1,10 +1,52 @@
 export const LESSONS = [
     {
-        lessonNumber: 1,
+        lessonNumber: '01',
         title: "Let's Get Started",
         backgroundColor: '#B6C1F0',
     },
     {
-        lessonNumber: 2,
+        lessonNumber: '02',
+        title: 'Elements panel',
+        backgroundColor: '#7FC2F4',
+    },
+    {
+        lessonNumber: '03',
+        title: 'Adding Elements',
+        backgroundColor: '#FA9DBC',
+    },
+    {
+        lessonNumber: '04',
+        title: 'Computed Styles',
+        backgroundColor: '#DCD9BF',
+    },
+    {
+        lessonNumber: '05',
+        title: 'Styles Panel',
+        backgroundColor: '#92D0AB',
+    },
+    {
+        lessonNumber: '06',
+        title: 'Creating Class',
+        backgroundColor: '#6A55A2',
+    },
+    {
+        lessonNumber: '07',
+        title: 'Components & Properties',
+        backgroundColor: '#A09D2A',
+    },
+    {
+        lessonNumber: '08',
+        title: 'Flex Layouts',
+        backgroundColor: '#F486C9',
+    },
+    {
+        lessonNumber: '09',
+        title: 'Grid Layouts',
+        backgroundColor: '#FF5B2B',
+    },
+    {
+        lessonNumber: '10',
+        title: 'Working With Git',
+        backgroundColor: '#FFB615',
     },
 ];
--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,21 +1,27 @@
 import type { MetaFunction } from '@remix-run/node';
 import styles from './home-page.module.scss';
 import { IntroductionCard } from '~/components/introduction-card/introduction-card';
+import { LESSONS } from './lessons';
 
 export const meta: MetaFunction = () => {
     return [{ title: 'Intro Tutorial' }, { name: 'description', content: 'Welcome to Remix!' }];
 };
 
 export default function HomePage() {
-    const items = [1, 2, 3, 4, 5, 6];
     return (
         <div className={styles.gridContainer}>
             <div className={`${styles.gridItem} ${styles.gridItemLarge}`}>
                 <IntroductionCard />
             </div>
-            {items.map((item, index) => (
-                <div key={index} className={styles.gridItem}>
-                    Item {item}
+            {LESSONS.map((item, index) => (
+                <div
+                    key={index}
+                    className={styles.gridItem}
+                    style={{ backgroundColor: `${item.backgroundColor}` }}
+                >
+                    <span />
+                    <p className={styles.cardTitle}>{item.title}</p>
+                    <p className={styles.cardNumber}>{`LESSON ${item.lessonNumber}`}</p>
                 </div>
             ))}
         </div>
