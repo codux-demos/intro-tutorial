31602f179785acf338eec5f77b535a6402e2793c
Codux <codux@codux.com>
2024-10-22 15:16:57 +0300

    Add links to lessons

--- app/routes/_index/lessons.ts
+++ app/routes/_index/lessons.ts
@@ -3,50 +3,60 @@ export const LESSONS = [
         lessonNumber: '01',
         title: "Let's Get Started",
         backgroundColor: '#B6C1F0',
+        url: '01-lets-start'
     },
     {
         lessonNumber: '02',
         title: 'Elements panel',
         backgroundColor: '#7FC2F4',
+        url: '02-elements-panel'
     },
     {
         lessonNumber: '03',
         title: 'Adding Elements',
         backgroundColor: '#FA9DBC',
+        url: '03-add-elements'
     },
     {
         lessonNumber: '04',
         title: 'Computed Styles',
         backgroundColor: '#DCD9BF',
+        url: '04-computed-styles'
     },
     {
         lessonNumber: '05',
         title: 'Styles Panel',
         backgroundColor: '#92D0AB',
+        url: '05-styles-panel'
     },
     {
         lessonNumber: '06',
         title: 'Creating Class',
         backgroundColor: '#6A55A2',
+        url: '06-create-class'
     },
     {
         lessonNumber: '07',
         title: 'Components & Properties',
         backgroundColor: '#A09D2A',
+        url: '07-components'
     },
     {
         lessonNumber: '08',
         title: 'Flex Layouts',
         backgroundColor: '#F486C9',
+        url: '08-flex-layouts'
     },
     {
         lessonNumber: '09',
         title: 'Grid Layouts',
         backgroundColor: '#FF5B2B',
+        url: '09-grid-layouts'
     },
     {
         lessonNumber: '10',
         title: 'Working With Git',
         backgroundColor: '#FFB615',
+        url: '10-work-with-git'
     },
 ];
--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -2,6 +2,7 @@ import type { MetaFunction } from '@remix-run/node';
 import styles from './home-page.module.scss';
 import { IntroductionCard } from '~/components/introduction-card/introduction-card';
 import { LESSONS } from './lessons';
+import { Link } from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
     return [{ title: 'Intro Tutorial' }, { name: 'description', content: 'Welcome to Remix!' }];
@@ -14,15 +15,16 @@ export default function HomePage() {
                 <IntroductionCard />
             </div>
             {LESSONS.map((item, index) => (
-                <div
-                    key={index}
-                    className={styles.gridItem}
-                    style={{ backgroundColor: `${item.backgroundColor}` }}
-                >
-                    <span />
-                    <p className={styles.cardTitle}>{item.title}</p>
-                    <p className={styles.cardNumber}>{`LESSON ${item.lessonNumber}`}</p>
-                </div>
+                <Link to={item.url} key={index}>
+                    <div
+                        className={styles.gridItem}
+                        style={{ backgroundColor: `${item.backgroundColor}` }}
+                    >
+                        <span />
+                        <p className={styles.cardTitle}>{item.title}</p>
+                        <p className={styles.cardNumber}>{`LESSON ${item.lessonNumber}`}</p>
+                    </div>
+                </Link>
             ))}
         </div>
     );
