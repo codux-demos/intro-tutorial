ff2d42e22b44b9c24733d9c99e1f5488953472b3
Codux <codux@codux.com>
2024-10-27 09:20:56 +0200

    Move footer to bottom and adjust svgs

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -3,6 +3,12 @@ import styles from './home-page.module.scss';
 import { IntroductionCard } from '~/components/introduction-card/introduction-card';
 import { LESSONS } from './lessons';
 import { Link } from '@remix-run/react';
+import CoduxLogo from '../../../src/assets/codux-logo.svg?react';
+import Facebook from '../../../src/assets/facebook.svg?react';
+import XLogo from '../../../src/assets/x.svg?react';
+import DiscordLogo from '../../../src/assets/discord.svg?react';
+import YoutubeLogo from '../../../src/assets/youtube.svg?react';
+import MediumLogo from '../../../src/assets/medium.svg?react';
 
 export const meta: MetaFunction = () => {
     return [{ title: 'Intro Tutorial' }, { name: 'description', content: 'Welcome to Remix!' }];
@@ -10,27 +16,44 @@ export const meta: MetaFunction = () => {
 
 export default function HomePage() {
     return (
-        <div className={styles.gridContainer}>
-            <div className={`${styles.gridItem} ${styles.gridItemLarge}`}>
-                <IntroductionCard />
+        <div className={styles.root}>
+            <div className={styles.gridContainer}>
+                <div className={`${styles.gridItem} ${styles.gridItemLarge}`}>
+                    <IntroductionCard />
+                </div>
+                {LESSONS.map((item, index) => (
+                    <Link to={item.url} key={index}>
+                        <div
+                            className={styles.gridItem}
+                            style={
+                                {
+                                    '--lesson-cover-image': item.lessonCoverImage,
+                                    '--lesson-bg-color': item.backgroundColor,
+                                } as React.CSSProperties
+                            }
+                        >
+                            <p className={styles.cardTitle}>{item.title}</p>
+                            <button className={styles.startLessonButton}>Start Lesson</button>
+                            <p className={styles.cardNumber}>{`LESSON ${item.lessonNumber}`}</p>
+                        </div>
+                    </Link>
+                ))}
             </div>
-            {LESSONS.map((item, index) => (
-                <Link to={item.url} key={index}>
-                    <div
-                        className={styles.gridItem}
-                        style={
-                            {
-                                '--lesson-cover-image': item.lessonCoverImage,
-                                '--lesson-bg-color': item.backgroundColor,
-                            } as React.CSSProperties
-                        }
-                    >
-                        <p className={styles.cardTitle}>{item.title}</p>
-                        <button className={styles.startLessonButton}>Start Lesson</button>
-                        <p className={styles.cardNumber}>{`LESSON ${item.lessonNumber}`}</p>
-                    </div>
-                </Link>
-            ))}
+            <footer className={styles.footer}>
+                <div className={styles.footerSection}>
+                    <CoduxLogo />
+                    <p>
+                        Powered by <span className={styles.boldText}>Codux</span>.
+                    </p>
+                </div>
+                <div className={styles.socials}>
+                    <XLogo />
+                    <Facebook />
+                    <DiscordLogo />
+                    <YoutubeLogo />
+                    <MediumLogo />
+                </div>
+            </footer>
         </div>
     );
 }
--- src/components/introduction-card/introduction-card.tsx
+++ src/components/introduction-card/introduction-card.tsx
@@ -1,10 +1,4 @@
 import styles from './introduction-card.module.scss';
-import CoduxLogo from '../../assets/codux-logo.svg?react';
-import Facebook from '../../assets/facebook.svg?react';
-import XLogo from '../../assets/x.svg?react';
-import DiscordLogo from '../../assets/discord.svg?react';
-import YoutubeLogo from '../../assets/youtube.svg?react';
-import MediumLogo from '../../assets/medium.svg?react';
 
 export const IntroductionCard = () => (
     <div className={styles.root}>
@@ -17,20 +11,5 @@ export const IntroductionCard = () => (
                 Did you manage to plug the holes?
             </p>
         </main>
-        <footer className={styles.footer}>
-            <div className={styles.footerSection}>
-                <CoduxLogo />
-                <p>
-                    Powered by <span className={styles.boldText}>Codux</span>.
-                </p>
-            </div>
-            <div className={styles.socials}>
-                <XLogo />
-                <Facebook />
-                <DiscordLogo />
-                <YoutubeLogo />
-                <MediumLogo />
-            </div>
-        </footer>
     </div>
 );
