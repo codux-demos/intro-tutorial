960c02856599e67d5dcf287d6bd158e16727d869
Codux <codux@codux.com>
2024-10-27 10:03:13 +0200

    Change description in the intro card

--- src/components/introduction-card/introduction-card.tsx
+++ src/components/introduction-card/introduction-card.tsx
@@ -1,14 +1,14 @@
 import styles from './introduction-card.module.scss';
+import { TaskSymbol } from '~/components/common/task-symbol/task-symbol';
 
 export const IntroductionCard = () => (
     <div className={styles.root}>
         <main className={styles.body}>
             <p className={styles.title}>Let’s start building with Codux!</p>
             <p className={styles.description}>
-                Just like with grid layout, you can use flex layout to arange elements. Let’s try
-                it: Select div.layer1 and then choose the .layer1 Selector in the Styles Panel.
+                Switch to <TaskSymbol name="preview" /> <b>Preview Mode</b> in the top right corner
+                of the stage. Then click on the first lesson to start the tutorial.
                 <br />
-                Did you manage to plug the holes?
             </p>
         </main>
     </div>
