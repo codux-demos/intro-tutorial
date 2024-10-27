b312be6ce39f1a9944fa5c39fc38e7da6e2d2ee1
matan georgi <matang@wix.com>
2024-10-27 15:47:56 +0200

    delete unnecessary board

--- src/_codux/boards/new-board.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import Lesson10 from '~/page/10-work-with-git/route';
-
-export default createBoard({
-    name: 'New Board',
-    Board: () => <Lesson10 />,
-});
