d0f0fab9c5528a35a35284ca20c3bd073b7ce251
Sagiv Dayan <sagiv.keeper@gmail.com>
2024-11-06 16:45:11 +0200

    style: add complex round checkbox and floating input

--- src/components/login-form/login-form.tsx
+++ src/components/login-form/login-form.tsx
@@ -11,21 +11,29 @@ export const LoginForm: React.FC<LoginFormProps> = ({ className }) => (
         <h2 className={styles.header1}>
             Welcome <br /> Back :)
         </h2>
-        <div>
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" />
-        </div>
-        <div>
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" />
-        </div>
-        <div>
-            <label>
-                <input type="checkbox" name="rememberMe" />
-                Remember me
+        <div className={styles.inputContainer}>
+            <input className={styles.floatingInput} type="text" id="username" name="username" />
+            <label className={styles.label} htmlFor="username">
+                Username:
             </label>
         </div>
-        <button type="submit">Sign In</button>
-        <a href="/forgot-password">Forgot your password?</a>
+        <br />
+        <div className={styles.inputContainer}>
+            <input className={styles.floatingInput} type="password" id="password" name="password" />
+            <label className={styles.label} htmlFor="password">
+                Password:
+            </label>
+        </div>
+        <label className={styles.roundCheckboxContainer}>
+            <input className={styles.roundCheckbox} type="checkbox" name="rememberMe" />
+            <span className={styles.checkmark}></span>
+        </label>
+        <span>Remember me next time!</span>
+        <button className={styles.confirmButton} type="submit">
+            Sign In
+        </button>
+        <a className={styles.forgotPassword} href="/forgot-password">
+            Forgot your password?
+        </a>
     </form>
 );
