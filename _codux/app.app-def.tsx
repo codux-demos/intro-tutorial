3f5b2a4c797fccb408902d9ec66900eb048b603b
PeterShershov <stelaviv@gmail.com>
2024-11-10 15:32:12 +0200

    feat: add a custom page template
    This PR also includes:
    - add error boundary
    - add height to header

--- _codux/app.app-def.tsx
+++ _codux/app.app-def.tsx
@@ -1,6 +1,87 @@
-import defineRemixApp from '@wixc3/define-remix-app';
+import defineRemixApp, { RouteExtraInfo } from '@wixc3/define-remix-app';
+import { type IReactApp } from '@wixc3/app-core';
 
-export default defineRemixApp({
+const app = defineRemixApp({
     appPath: '../app',
     routingPattern: 'folder(route)',
 });
+
+const NEW_PAGE_TEMPLATE = `
+import React from 'react';
+
+const boxes = [
+    '#ffffff',
+    '#dcd9bf',
+    '#f5f5f5',
+    '#000000',
+    '#282b2d',
+    '#b6c1f0',
+    '#cc6fd6',
+    '#6a55a2',
+    '#6a55a2bc',
+    '#a09d2a',
+    '#268040',
+    '#92d0ab',
+    '#7fc2f4',
+    '#ffb615',
+    '#ffd9e0',
+    '#f486c9',
+    '#fa9dbc',
+    '#ff2f2f',
+    '#ff5b2b',
+    '#cccccc',
+    '#3899ec',
+    '#495cef',
+    '#e7eaff',
+    '#fcfcfc',
+];
+
+export default function NewPage() {
+    return (
+        <main className="newPage">
+            {Array.from({length:  75}).map((_, idx) => (
+                <div
+                    key={idx}
+                    className="box"
+                    style={{
+                        backgroundColor: randomItemFromArray(boxes),
+                        width: \`\${randomNumberInRange(50, 180)}px\`,
+                        height: '60px',
+                        transform: \`rotate(\${randomNumberInRange(-45, 45)}deg)\`,
+                        animationDelay: \`\${idx * 0.05}s\`,
+                        top: \`\${randomNumberInRange(0, 95)}vh\`,
+                        left: \`\${randomNumberInRange(0, 95)}vw\`,
+                    }}
+                />
+            ))}
+            <h1>Awesome!</h1>
+            <h2>You have just created your first page.</h2>
+            <h2>Now, the sky is the limit ✨</h2>
+        </main>
+    );
+}
+
+function randomItemFromArray<T>(array: T[]): T {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+function randomNumberInRange(min: number, max: number): number {
+    return Math.random() * (max - min) + min;
+}
+`;
+
+export default {
+    ...app,
+    getNewPageInfo(options) {
+        const newPageInfo = app.getNewPageInfo?.(options);
+
+        if (!newPageInfo) {
+            return;
+        }
+
+        return {
+            ...newPageInfo,
+            newPageSourceCode: NEW_PAGE_TEMPLATE,
+        };
+    },
+} as IReactApp<RouteExtraInfo>;
