b8c98ef2c953daa66da40a6c78fc9753dc6f6468
matangeorgi <87900560+matangeorgi@users.noreply.github.com>
2024-11-12 10:47:00 +0200

    chore: update deps and pull changes from main (#241)

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
     );
 }
 
+export function ErrorBoundary() {
+    return <div>Error Boundary</div>;
+}
+
 export default function App() {
     return <Outlet />;
 }
