710a2ae4330da93b59ccb2790be88b932e0d85c0
Peter Shershov <16524839+PeterShershov@users.noreply.github.com>
2024-11-14 11:39:43 +0200

    style: fix home-screen style to be responsive (#244)

--- _codux/app.app-def.tsx
+++ _codux/app.app-def.tsx
@@ -8,40 +8,18 @@ const app = defineRemixApp({
 
 const NEW_PAGE_TEMPLATE = `
 import React from 'react';
-import { boxes } from '../../constants';
 import vars from '~/globals/variables.module.scss';
+import Boxes from '../../../src/assets/boxes.svg?react';
 
 export default function NewPage() {
     return (
         <main className="newPage">
-            {Array.from({length:  75}).map((_, idx) => (
-                <div
-                    key={idx}
-                    className="box"
-                    style={{
-                        backgroundColor: vars[randomItemFromArray(boxes)],
-                        width: \`\${randomNumberInRange(50, 180)}px\`,
-                        height: '60px',
-                        transform: \`rotate(\${randomNumberInRange(-45, 45)}deg)\`,
-                        animationDelay: \`\${idx * 0.05}s\`,
-                        top: \`\${randomNumberInRange(0, 95)}vh\`,
-                        left: \`\${randomNumberInRange(0, 95)}vw\`,
-                    }}
-                />
-            ))}
+            <Boxes className="boxes" />
             <h1>Awesome!</h1>
             <h2>You have just created your own page.</h2>
             <h2>Now, the sky is the limit ✨</h2>
         </main>
     );
-}
-
-function randomItemFromArray<T>(array: T[]): T {
-    return array[Math.floor(Math.random() * array.length)];
-}
-
-function randomNumberInRange(min: number, max: number): number {
-    return Math.random() * (max - min) + min;
 }
 `;
 
--- app/routes/_index/lessons.tsx
+++ app/routes/_index/lessons.tsx
@@ -3,89 +3,157 @@ import { PagesOverlay } from './pages-overlay';
 
 interface Lesson {
     lessonNumber: string;
-    title: string;
+    header: string;
+    title: JSX.Element;
     backgroundColor: string;
     lessonCoverImage: string;
     url?: string;
     lessonContent?: React.ComponentType;
+    footerTextColor: string;
 }
 
 export const LESSONS: Lesson[] = [
     {
         lessonNumber: '01',
-        title: "Let's Get Started",
+        header: "Let's Get Started",
+        title: (
+            <>
+                Let's Get <br /> Started
+            </>
+        ),
         backgroundColor: style.babyPink,
         url: '01-lets-start',
         lessonCoverImage: 'url(../../../src/assets/lesson-01-cover.svg)',
+        footerTextColor: style.charcoalBlack,
     },
     {
         lessonNumber: '02',
-        title: 'Elements panel',
+        header: 'Elements Panel',
+        title: (
+            <>
+                Elements <br /> Panel
+            </>
+        ),
         backgroundColor: style.goldYellow,
         url: '02-elements-panel',
         lessonCoverImage: 'url(../../../src/assets/lesson-02-cover.svg)',
+        footerTextColor: style.white,
     },
     {
         lessonNumber: '03',
-        title: 'Adding Elements',
+        header: 'Add Elements',
+        title: (
+            <>
+                Add <br /> Elements
+            </>
+        ),
         backgroundColor: style.aeroBlue,
         url: '03-add-elements',
         lessonCoverImage: 'url(../../../src/assets/lesson-03-cover.svg)',
+        footerTextColor: style.white,
     },
     {
         lessonNumber: '04',
-        title: 'Computed Styles',
+        header: 'Computed Styles',
+        title: (
+            <>
+                Computed <br /> Styles
+            </>
+        ),
         backgroundColor: style.lavender,
         url: '04-computed-styles',
-        lessonCoverImage: 'url(../../../src/assets/lesson-04-cover.svg)',
+        lessonCoverImage: 'url(../../../src/assets/lesson-04-cover.jpg)',
+        footerTextColor: style.white,
     },
     {
         lessonNumber: '05',
-        title: 'Styles Panel',
+        header: 'Styles Panel',
+        title: (
+            <>
+                Styles <br /> Panel
+            </>
+        ),
         backgroundColor: style.pastelPink,
         url: '05-styles-panel',
-        lessonCoverImage: 'url(../../../src/assets/lesson-05-cover.jpg)',
+        lessonCoverImage: 'url(../../../src/assets/lesson-05-cover.svg)',
+        footerTextColor: style.white,
     },
     {
         lessonNumber: '06',
-        title: 'Creating Class',
+        header: 'Creating Class',
+        title: (
+            <>
+                Creating <br /> Class
+            </>
+        ),
         backgroundColor: style.turquoiseGreen,
         url: '06-create-class',
         lessonCoverImage: 'url(../../../src/assets/lesson-06-cover.svg)',
+        footerTextColor: style.white,
     },
     {
         lessonNumber: '07',
-        title: 'Components & Properties',
+        header: 'Components & Properties',
+        title: (
+            <>
+                Components & <br /> Properties
+            </>
+        ),
         backgroundColor: style.darkLavender80,
         url: '07-components',
         lessonCoverImage: 'url(../../../src/assets/lesson-07-cover.svg)',
+        footerTextColor: style.white,
     },
     {
         lessonNumber: '08',
-        title: 'Flex Layouts',
+        header: 'Flex Layouts',
+        title: (
+            <>
+                Flex <br /> Layouts
+            </>
+        ),
         backgroundColor: style.oliveGreen,
         url: '08-flex-layouts',
         lessonCoverImage: 'url(../../../src/assets/lesson-08-cover.svg)',
+        footerTextColor: style.white,
     },
     {
         lessonNumber: '09',
-        title: 'Grid Layouts',
+        header: 'Grid Layouts',
+        title: (
+            <>
+                Grid <br /> Layouts
+            </>
+        ),
         backgroundColor: style.dirtyWhite,
         url: '09-grid-layouts',
         lessonCoverImage: 'url(../../../src/assets/lesson-09-cover.svg)',
+        footerTextColor: style.charcoalBlack,
     },
     {
         lessonNumber: '10',
-        title: 'Working With Git',
+        header: 'Working with Git',
+        title: (
+            <>
+                Working with <br /> Git
+            </>
+        ),
         backgroundColor: style.orangeRed,
         url: '10-work-with-git',
         lessonCoverImage: 'url(../../../src/assets/lesson-10-cover.svg)',
+        footerTextColor: style.charcoalBlack,
     },
     {
         lessonNumber: '11',
-        title: 'Adding Pages',
+        header: 'Adding Pages',
+        title: (
+            <>
+                Adding <br /> Pages
+            </>
+        ),
         backgroundColor: style.darkGreen,
         lessonCoverImage: 'url(../../../src/assets/lesson-11-cover.svg)',
         lessonContent: PagesOverlay,
+        footerTextColor: style.charcoalBlack,
     },
 ];
