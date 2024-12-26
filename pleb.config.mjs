a05cba8e8c761c28660d85e33a0525fa377ca97b
Noam Musba <noammu@wix.com>
2024-12-26 12:29:29 +0200

    chore(deps): upgrade and add pleb ignore We will ignore everything related to `React` v19, `React Router` v7. Also, `Vite` v6 is having problems with `"@remix-run/dev"` so we can't upgrade them also.

---
+++ pleb.config.mjs
@@ -0,0 +1,10 @@
+export default {
+    pinnedPackages: [
+        { name: 'vite', reason: 'incompatible with @remix-run/dev' },
+        { name: 'react', reason: 'we are not ready for react v19 yet' },
+        { name: 'react-dom', reason: 'we are not ready for react v19 yet' },
+        { name: '@types/react', reason: 'we are not ready for react v19 yet' },
+        { name: '@types/react-dom', reason: 'we are not ready for react v19 yet' },
+        { name: 'remix-utils', reason: 'uses react router v7 and we are incompatible with it' },
+    ],
+};
