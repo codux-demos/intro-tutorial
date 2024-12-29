9730ef0bd555a9b1c0987508bd6bbeb767e6e37d
Noam Musba <noammu@wix.com>
2024-12-29 12:46:26 +0200

    fix: remove unwanted pleb config file

--- pleb.config.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-    pinnedPackages: [
-        { name: 'react', reason: 'we are not ready for react v19 yet' },
-        { name: 'react-dom', reason: 'we are not ready for react v19 yet' },
-        { name: '@types/react', reason: 'we are not ready for react v19 yet' },
-        { name: '@types/react-dom', reason: 'we are not ready for react v19 yet' },
-        { name: 'remix-utils', reason: 'uses react router v7 and we are incompatible with it' },
-    ],
-};
